8ac54532c3d7978522ea5c814b7daf32f99d7959
Mossa Hana <mhana1@hawk.iit.edu>
2015-11-04 22:37:37 -0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -8,21 +8,21 @@ mapfile -t instArr < <(aws ec2 run-instances --image-id $1 --count $2 --instance
 
 echo ${instArray[@]}
 
-aws ec2 wait instance-running --instance-ids $ ${instArray[@]}
+aws ec2 wait instance-running --instance-ids ${instArray[@]}
 echo "instances are running"
 
 #create load balancer
-aws elb create-load-balancer --load-balancer-name MP1-lb --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --security-groups $4 --subnets $5 --output=text
+ELBURL=('aws elb create-load-balancer --load-balancer-name MP1-lb --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --security-groups $4 --subnets $5 --output=text');
+echo $ELBURL
 
 echo -e "\nFinished launching ELB and sleeping 25 second"
-for i in {0..3}; do echo -ne '.'; sleep 1;done
+for i in {0..25}; do echo -ne '.'; sleep 1;done
 echo "\n"
 
 #register load balancer
 aws elb register-instances-with-load-balancer --load-balancer-name MP1-lb --instances ${instArray[@]}
 
-
-aws elb configure-health-check --load-balancer-name MP1-lb --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+aws elb configure-health-check --load-balancer-name MP1-lb --health-check Target=HTTP:80/index.html,Interval=50,UnhealthyThreshold=3,HealthyThreshold=3,Timeout=4
 
 echo -e "\nWaiting an additional 3 minutes (180 seconds) . before opening the ELB in a webbrowser"
 for i in {0..3}; do echo -ne '.'; sleep 1;done
@@ -33,5 +33,5 @@ aws autoscaling create-launch-configuration --launch-configuration-name itmo544-
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-extended-auto-scaling-group-2 --launch-configuration-name itmo544-launch-config --load-balancer-names MP1-lb  --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5
 
 
-aws rds create-db-instance --db-instance-identifier mh-db --db-instance-class db.t1.micro --engine MySQL --allocated-storage 5 --master-username controller --master-user-password letmein888
+aws rds create-db-instance --db-instance-identifier mh-db --db-name mhana1DB --db-instance-class db.t1.micro --engine MySQL --allocated-storage 5 --master-username controller --master-user-password letmein888
 	 
